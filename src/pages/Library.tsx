aff9b2b6f80b43ccc0857683be5aacaa17d43a5a
unknown <ea.avdeev@gmail.com>
2024-04-19 22:34:34 +0300

    feat(library page): search bar

--- src/pages/Library.tsx
+++ src/pages/Library.tsx
@@ -2,12 +2,18 @@ import React, { useEffect, useState } from 'react';
 import Layout from '../components/Layout';
 import MainSection from '../components/MainSection';
 import Card from '../components/Card';
+import SearchBar from '../components/SearchBar';
 import { fetchPosts } from '../api/postsApi';
 import { fetchComments } from '../api/commentsApi';
 
 const Library: React.FC = () => {
   const [posts, setPosts] = useState([]);
   const [comments, setComments] = useState([]);
+
+  const handleSearch = (searchTerm: string) => {
+    // Ваша логика поиска
+    console.log('Поиск слова:', searchTerm);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -22,8 +28,12 @@ const Library: React.FC = () => {
 
   return (
     <Layout>
+
       <MainSection>
         <h1>Library_page</h1>
+
+        <SearchBar onSearch={handleSearch}/>
+
         {posts.map((post: any) => (
           <Card key={post.id} title={post.title} body={post.body} />
         ))}
